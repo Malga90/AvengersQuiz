91ad1f35aeb33b12cae32578edc045e4048ad2a2
Malga90 <savemeebarry@gmail.com>
2019-02-18 21:44:47 +0100

    added click event for all buttons

--- dist/js/script.min.js
+++ dist/js/script.min.js
@@ -1,34 +1,37 @@
 const pages = document.querySelectorAll('.pt-page');
-const nextBtn = document.querySelector('.btn');
+const buttons = document.querySelectorAll('.btn');
 
 let current = 0;
 
-nextBtn.addEventListener('click', function(e) {
-	e.preventDefault();
-
-	if (current >= 5) {
-		return false;
-	}
-
-
-	pages[current].classList.add("pt-page-rotateCubeLeftOut");
-	pages[current].classList.add("pt-page-ontop");
-
-	pages[current + 1].classList.add("pt-page-rotateCubeLeftIn");
-	pages[current + 1].classList.add("pt-page-current");
-
-	console.log(current);
-	current++
-
-	setTimeout(() => {
-	pages[current].classList.remove("pt-page-rotateCubeLeftIn");
-	pages[current - 1].classList.remove("pt-page-current");
-	pages[current - 1].classList.remove("pt-page-rotateCubeLeftOut");
-	pages[current - 1].classList.remove("pt-page-ontop");
-
-	
-}, 600);
-
-console.log(current);
+buttons.forEach(function(currentBtn){
+    currentBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    if (current >= 5) {
+        return false;
+    }
+    
+    
+    pages[current].classList.add("pt-page-rotateCubeLeftOut");
+    pages[current].classList.add("pt-page-ontop");
+    
+    pages[current + 1].classList.add("pt-page-rotateCubeLeftIn");
+    pages[current + 1].classList.add("pt-page-current");
+    
+    console.log(current);
+    current++;
+    
+    setTimeout(() => {
+    pages[current].classList.remove("pt-page-rotateCubeLeftIn");
+    pages[current - 1].classList.remove("pt-page-current");
+    pages[current - 1].classList.remove("pt-page-rotateCubeLeftOut");
+    pages[current - 1].classList.remove("pt-page-ontop");
+    
+        
+    }, 600);
+    
+    console.log(current);
+    
+    });
 
 });
