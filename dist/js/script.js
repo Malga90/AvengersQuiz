ba46464676a92ec989755a1862c654996cd4fe3a
Malga90 <savemeebarry@gmail.com>
2019-02-18 21:13:37 +0100

    cube rotation to the left

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,19 +11,19 @@ nextBtn.addEventListener('click', function(e) {
 	}
 
 
-	pages[current].classList.add("pt-page-rotateCubeRightOut");
+	pages[current].classList.add("pt-page-rotateCubeLeftOut");
 	pages[current].classList.add("pt-page-ontop");
 
-	pages[current + 1].classList.add("pt-page-rotateCubeRightIn");
+	pages[current + 1].classList.add("pt-page-rotateCubeLeftIn");
 	pages[current + 1].classList.add("pt-page-current");
 
 	console.log(current);
 	current++
 
 	setTimeout(() => {
-	pages[current].classList.remove("pt-page-rotateCubeRightIn");
+	pages[current].classList.remove("pt-page-rotateCubeLeftIn");
 	pages[current - 1].classList.remove("pt-page-current");
-	pages[current - 1].classList.remove("pt-page-rotateCubeRightOut");
+	pages[current - 1].classList.remove("pt-page-rotateCubeLeftOut");
 	pages[current - 1].classList.remove("pt-page-ontop");
 
 	
