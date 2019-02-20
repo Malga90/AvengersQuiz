1a78f39c1ed2c481182b178a8e46be0dcce3e08f
Malga90 <savemeebarry@gmail.com>
2019-02-20 21:55:29 +0100

    html template in javascript

--- dist/js/script.min.js
+++ dist/js/script.min.js
@@ -6,7 +6,8 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/1.jpg'
     },
     {
         question: '2. How are you spending your free time?',
@@ -15,7 +16,9 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/2.jpg'
+        
     },
     {
         question: '3. How are you spending your free time?',
@@ -24,7 +27,8 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/3.jpg'
     },
     {
         question: '4. How are you spending your free time?',
@@ -33,7 +37,8 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/4.jpg'
     },
     {
         question: '5. How are you spending your free time?',
@@ -42,7 +47,8 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/5.jpg'
     },
     {
         question: '6. How are you spending your free time?',
@@ -51,7 +57,8 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/6.jpg'
     },
     {
         question: '7. How are you spending your free time?',
@@ -60,7 +67,8 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/7.jpg'
     },
     {
         question: '8. How are you spending your free time?',
@@ -69,7 +77,8 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/8.jpg'
     },
     {
         question: '9. How are you spending your free time?',
@@ -78,7 +87,8 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/9.jpg'
     },
     {
         question: '10. How are you spending your free time?',
@@ -87,54 +97,114 @@ const quiz = [
         answer3: 'Smashing things and people',
         answer4: 'Quality time with my family',
         answer5: 'Gym',
-        answer6: 'Partyyy!'
+        answer6: 'Partyyy!',
+        image: 'src/img/10.jpg'
     }
 ];
 
-// quiz.forEach((data, idx) => {
-//     let markup = createSeries(data, idx);
-//     let section = document.createElement("section");
-//     section.classList.add("section__question");
-//     section.innerHTML = markup;
-//     document.header.appendChild(section);
-// });
+quiz.forEach((data, idx) => {
+    const markup = createSeries(data, idx);
+    const section = document.createElement("section");
+    const container = document.querySelector('#pt-main');
+    
+    section.classList.add("section__question");
+    section.classList.add("pt-page");
+    section.innerHTML = markup;
+    container.appendChild(section);
+});
 
-// function createSeries(data, idx) {
-//     return `
-//       <h2 class="a-Series_Title">${datarecord.Title}</h2>
-//       <p class="a-Series_Description">
-//         <span class="a-Series_DescriptionHeader">Description: </span>${datarecord.Description}
-//       </p>
-//   <h4 class="a-EpisodeBlock_Title">First episodes</h4>
-//   <ul class="a-EpisodeBlock">    
-//       <li class="a-EpisodeBlock_Item">
-  
-//         ${datarecord.Episodes.map((episode, index) => 
-//           `
-              
-//               <span class="a-EpisodeBlock_Title"><b class="a-EpisodeBlock_EpisodeNo">${index+1}</b> ${episode}</span>
-          
-//           `
-//         ).join("")}
-  
-  
-//       </li>
-//       ${datarecord.Ended === true ? `` : `<div class="a-Series_More">More to come!</div>`}
-//   </ul>
-//   <h4 class="a-ReviewsBlock_Title">Reviews</h4>
-//       <ul class="a-ReviewsBlock">
-          
-//   ${datarecord.Reviews.map(review => 
-//           `<li class="a-ReviewsBlock_Origin">
-//               <b class="a-ReviewsBlock_Reviewer">${Object.keys(review)[0]}</b>
-//               <span class="a-ReviewsBlock_Score">${review[Object.keys(review)[0]]}%</span>
-//           </li>
-//           `
-//         ).join("")}
-//       </ul>
-//       <div class="a-UserRating">Average user rating: <b class="a-UserRating_Score">${getRatingsAverage(datarecord.UserRatings)}</b></div>
-//   `;
-// }
+function createSeries(data, idx) {
+    if (idx % 2 === 0) {
+        return `
+            <div class='row'>
+                <div class='column'>
+                <div class='question-column'>
+                    <div class="question-box">
+                        <h2 class="question-heading">
+                        ${data.question}
+                        </h2>
+                        <ul class="answers__list">
+                            <label>
+                                <input type="radio" name="one" value="a"><li class="answers__list-item">${data.answer1}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="b"><li class="answers__list-item">${data.answer2}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="c"><li class="answers__list-item">${data.answer3}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="d"><li class="answers__list-item">${data.answer4}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="e"><li class="answers__list-item">${data.answer5}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="f"><li class="answers__list-item">${data.answer6}</li>
+                            </label>
+                        </ul>
+                    </div>
+                    <div class="box-btn-next">
+                        <a href="#" class="btn btn-next">Next<span class="arrow-right">&rArr;</span></a> 
+                    </div>
+                </div>
+                </div>
+                <div class='column'>
+                <div class='image-column'>
+                    <div class="image-box">
+                        <img src="${data.image}" alt="Avengers" class="image">
+                    </div>
+                </div>
+                </div>
+        </div>
+        `;
+    } else {
+        return `
+            <div class='row'>
+                <div class='column'>
+                <div class='image-column'>
+                    <div class="image-box">
+                        <img src="${data.image}" alt="Avengers" class="image">
+                    </div>
+                </div>
+                </div>
+                <div class='column'>
+                <div class='question-column'>
+                    <div class="question-box">
+                        <h2 class="question-heading">
+                        ${data.question}
+                        </h2>
+                        <ul class="answers__list">
+                            <label>
+                                <input type="radio" name="one" value="a"><li class="answers__list-item">${data.answer1}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="b"><li class="answers__list-item">${data.answer2}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="c"><li class="answers__list-item">${data.answer3}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="d"><li class="answers__list-item">${data.answer4}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="e"><li class="answers__list-item">${data.answer5}</li>
+                            </label>
+                            <label>
+                                <input type="radio" name="one" value="f"><li class="answers__list-item">${data.answer6}</li>
+                            </label>
+                        </ul>
+                    </div>
+                    <div class="box-btn-next">
+                        <a href="#" class="btn btn-next">Next<span class="arrow-right">&rArr;</span></a> 
+                    </div>
+                </div>
+                </div>
+            </div>
+        `;
+    }
+    
+};
 
 
 const pages = document.querySelectorAll('.pt-page');
